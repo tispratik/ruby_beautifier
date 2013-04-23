ded4924308c2571638284f1ee3e293449e95b44d
Pratik Khadloya <tispratik@gmail.com>
2013-04-23 10:04:02 -0700

    Some live examples

--- spec/ruby_beautifier_spec.rb
+++ spec/ruby_beautifier_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe RubyBeautifier do
 
   describe '#missing_space_before_hash_rocket' do
+    # http://rubular.com/r/TGwSyNOveS
+
     it 'a=>b should convert to a =>b' do
       text = 'my test a=>b string'
       RubyBeautifier.missing_space_before_hash_rocket(text).should == 'my test a =>b string'
@@ -24,6 +26,17 @@ describe RubyBeautifier do
     end
   end
 
+  describe '#missing_space_after_hash_rocket' do
+
+  end
+
+  describe '#missing_space_after_comma' do
+    # http://rubular.com/r/WjrwSnmzyP
+  end
+
+  describe '#missing_new_line_after_method' do
+    # http://rubular.com/r/hROOd59cmg
+  end
 end
 
 # Add space after comma, following cases have been tested
